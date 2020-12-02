2674cfd130abde14380c198a575875f8b7c0cc4d
roan1088 <rohan_188@yahoo.com>
2020-12-01 22:24:52 -0500

    Added a main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main></Main>
     </div>
   );
 }
